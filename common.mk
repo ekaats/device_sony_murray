acf5bee9106ecd11bb705974c7d9c5f8e199a543
Albert I <kras@raphielgang.org>
2022-11-14 21:08:30 +0000

    murray: inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.

--- common.mk
+++ common.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
     SonySagamiTelephonyResCommon \
     WifiResCommon
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
